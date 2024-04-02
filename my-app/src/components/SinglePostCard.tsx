16eda44890b0e5b779e8717e6241f272adb1355a
grzeg <grzegorzterenda@gmail.com>
2024-04-02 19:48:05 +0200

    feat: Split SinglePostCard component into SinglePostCardImg, SinglePostCardDetails and Tags for better organization and reusability.

--- my-app/src/components/SinglePostCard.tsx
+++ my-app/src/components/SinglePostCard.tsx
@@ -1,30 +1,15 @@
-import { PostEntity } from "types";
-import {FaRegCalendar} from "react-icons/fa";
-import {formatDate} from "../utils/formatDate";
+import {PostEntity} from "types";
+import {SinglePostCardImg} from "./SinglePostCardImg";
+import {SinglePostCardDetails} from "./SinglePostCardDetails";
 
-export const SinglePostCard = ({ post }: { post: PostEntity }) => {
-    const { imageUrl, createdAt, title } = post;
+export const SinglePostCard = ({post}: { post: PostEntity }) => {
+    const {imageUrl, createdAt, title, tags} = post;
 
     return (
-        <article className="w-80 border border-gray-300 rounded-br-xl overflow-hidden shadow-lg flex flex-wrap bg-white rounded-br-4xl dark:border-neutral-800 dark:shadow-neutral-500 dark:shadow-md">
-            <div className="bg-amber-400 h-40 w-full overflow-hidden bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
-                <img
-                    src={imageUrl || 'https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2021/09/how-to-write-a-blog-post.png'}
-                    alt="xD" className="w-full h-full object-cover" />
-            </div>
-            <div className="w-full flex dark:bg-neutral-900">
-                <div className="p-6 w-2/3">
-                    <h5 className="mb-2 block text-xl">
-                        {title || 'Test title'}
-                    </h5>
-                    <div className="mr-auto mt-1 flex">
-                        <span className="flex items-center text-xs gap-1 ">
-                            <FaRegCalendar />
-                            {!createdAt ? '01.02.03' : formatDate(createdAt)}
-                        </span>
-                    </div>
-                </div>
-            </div>
+        <article
+            className="w-80 h-80 border border-gray-300 rounded-br-3xl overflow-hidden shadow-lg flex flex-wrap bg-white dark:border-neutral-800 dark:shadow-neutral-500 dark:shadow-md">
+            <SinglePostCardImg title={title} imageUrl={imageUrl}/>
+           <SinglePostCardDetails tags={tags} title={title} createdAt={createdAt}/>
         </article>
     )
 }
