9b894a746791ec9dcb1efbbada61c3583c447eb1
grzeg <grzegorzterenda@gmail.com>
2024-05-15 21:01:32 +0200

    Refactored code for Blog page: -moved blog header title to separated component

---
+++ my-app/src/components/BlogHeaderTitle.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const BlogHeaderTitle = () => {
+    return <>
+        <div className="flex flex-col my-10">
+            <h1 className="text-6xl">Blog Readme</h1>
+            <p className="text-2xl my-1">My road to become Software Engineer</p>
+        </div>
+    </>
+}
