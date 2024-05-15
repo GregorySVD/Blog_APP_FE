412265c580ddc8efaaa5bdc1d9fcb958782a6dcc
grzeg <grzegorzterenda@gmail.com>
2024-05-15 20:48:57 +0200

    Fixed place of display of loader

--- my-app/src/components/common/LoaderSpinner.tsx
+++ my-app/src/components/common/LoaderSpinner.tsx
@@ -4,6 +4,9 @@ import React from 'react';
 export const LoaderSpinner = () => {
 
     return <>
-        <span className="loading loading-spinner loading-lg"></span>
+        <div className="w-full flex justify-center h-full">
+            <span className="loading loading-spinner loading-lg text-6xl"></span>
+        </div>
+
     </>
 }
